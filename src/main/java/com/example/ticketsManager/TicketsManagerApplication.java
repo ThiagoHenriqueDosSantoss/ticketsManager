7d80d3730045d27b74bd575dcece09f2d4564f48
Thiago Henrique Dos Santos <thiaghenrique145@gmail.com>
2025-05-07 14:38:25 -0300

    Feat: created project

---
+++ src/main/java/com/example/ticketsManager/TicketsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticketsManager/TicketsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
