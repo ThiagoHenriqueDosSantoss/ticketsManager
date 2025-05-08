b728fe67de46a6a7c258dfa718acb76e843ed2b6
Th-henrii <thiaghenrique145@gmail.com>
2025-05-08 01:21:38 -0300

    Feat: method in controller for uptade Ticket

--- src/main/java/com/example/ticketsManager/controller/TicketController.java
+++ src/main/java/com/example/ticketsManager/controller/TicketController.java
@@ -1,18 +1,17 @@
 package com.example.ticketsManager.controller;
 
 import com.example.ticketsManager.dto.CreateTicketDTO;
+import com.example.ticketsManager.dto.UpdateTicketDTO;
 import com.example.ticketsManager.entities.Ticket;
 import com.example.ticketsManager.entities.User;
 import com.example.ticketsManager.repository.UserRepository;
 import com.example.ticketsManager.services.TicketService;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
@@ -33,6 +32,17 @@ public class TicketController {
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             logger.severe("Falha ao criar ticket no controller!");
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @PatchMapping(value = "/{idTicket}")
+    public ResponseEntity<Ticket> updateTicket(@PathVariable Long idTicket, @RequestBody UpdateTicketDTO dto){
+        try{
+            Ticket response = ticketService.uptadeTicket(idTicket,dto);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            logger.severe("ERROR: Falha ao atualizar ticket em controller!" + e.getMessage());
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
